0ed6ffcd581bf49a1bdcb1079b188eb00a48546b
bhaiyu <niksharma34@gmail.com>
2024-03-14 21:21:46 +0530

    Added jwt create and verify token

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,8 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
 const UserRepository = require('../repository/user-repository');
+const { JWT_KEY } = require('../config/serverConfig');
 
 class UserService {
     constructor() {
@@ -14,6 +18,28 @@ class UserService {
             throw error;
         }
     }
+
+    createToken(user) {
+        try {
+            const result = jwt.sign(user, JWT_KEY, {expiresIn: '1d'});//after expiredTime u can;t veryful a token
+            return result;
+        } catch (error) {
+            console.log("Something went wrong in token creation");
+            throw error;
+        }
+    }
+
+    verifyToken(token) {
+        try {
+            const response = jwt.verify(token, JWT_KEY);
+            return response;
+        } catch (error) {
+            console.log("Something went wrong in token validation", error);
+            throw error;
+        }
+    }
+
+
 }
 
 module.exports = UserService;
