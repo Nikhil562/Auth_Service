86f5ab79c812f9d0cc142cbde8b8db10cba152ff
Nikhil562 <nikhilsharmacfc@gmail.com>
2024-07-17 11:25:30 +0530

    Added user isAuth function

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -18,5 +18,9 @@ router.post(
     AuthRequestValidators.validateUserAuth,
     UserController.signIn
 );
+router.get(
+    '/isAuthenticated',
+    UserController.isAuthenticated
+)
 
 module.exports = router;
